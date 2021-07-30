b71bb3dd3d73a36a642d2b3008b758e08f130dee
Alex <math.sqrta@gmail.com>
2021-07-30 16:49:48 +0200

    refactoring
    Changed slider structure
    Removed outer elements 
    todo: 
    slide fixation
    scroller 
    footer

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-document.addEventListener('touchstart', handleTouchStart, false);        
-document.addEventListener('touchmove', handleTouchMove, false);
+// document.addEventListener('touchstart', handleTouchStart, false);        
+// document.addEventListener('touchmove', handleTouchMove, false);
 const first_item = document.getElementById('first-item');
 const second_item = document.getElementById('second-item');
 const third_item = document.getElementById('third-item');
@@ -10,93 +10,76 @@ const modal_window = document.getElementById('modal-one');
 
 //logo 
 
-// function classChange(item) {
-//   first_item.classList.add('sliders__item_active');
-//   item.classList.remove('sliders__item_active');
-//   item.classList.add('sliders__item_not-active');
-//   content.classList.add('sliders_first');
-//   content.classList.remove('sliders_second')
-// }; -- не успел
+// logo.addEventListener('click', (e) => {
+//   if(second_item.classList.contains('sliders__item_active')) {
+//     first_item.classList.add('sliders__item_active');
+//     second_item.classList.remove('sliders__item_active');
+//     second_item.classList.add('sliders__item_not-active');
+//     content.classList.add('sliders_first');
+//     content.classList.remove('sliders_second')
+//   } else if (third_item.classList.contains('sliders__item_active')) {
+//     first_item.classList.add('sliders__item_active');
+//     third_item.classList.remove('sliders__item_active');
+//     third_item.classList.add('sliders__item_not-active');
+//     content.classList.add('sliders_first');
+//     content.classList.remove('sliders_third');
+//   } else {
+//     return;
+//   }
+// })
 
-logo.addEventListener('click', () => {
-  if(second_item.classList.contains('sliders__item_active')) { //are we on the second slide?
-    first_item.classList.add('sliders__item_active');
-    second_item.classList.remove('sliders__item_active');
-    content.classList.add('sliders_first');
-    content.classList.remove('sliders_second')
-  } else if (third_item.classList.contains('sliders__item_active')) { //are we on the third slide?
-    first_item.classList.add('sliders__item_active');
-    third_item.classList.remove('sliders__item_active');
-    content.classList.add('sliders_first');
-    content.classList.remove('sliders_third');
-  } else {
-    return;
-  }
-})
+// //first button 
 
-//first button 
-
-first_button.addEventListener('click', () => {
-  first_item.classList.remove('sliders__item_active');
-  second_item.classList.add('sliders__item_active');
-  content.classList.remove('sliders_first');
-  content.classList.add('sliders_second')
-})
+// first_button.addEventListener('click', () => {
+//     first_item.classList.remove('sliders__item_active');
+//     second_item.classList.remove('sliders__item_not-active');
+//     second_item.classList.add('sliders__item_active');
+//     content.classList.remove('sliders_first');
+//     content.classList.add('sliders_second')
+// })
 
 //swipe
 
-let xDown = null;                                                        
-let yDown = null;
+// let xDown = null;                                                        
+// let yDown = null;
 
-function getTouches(evt) {
-  return evt.touches ||             
-         evt.originalEvent.touches; 
-}                                                     
+// function getTouches(evt) {
+//   return evt.touches ||             
+//          evt.originalEvent.touches; 
+// }                                                     
 
-function handleTouchStart(evt) {
-    const firstTouch = getTouches(evt)[0];                                      
-    xDown = firstTouch.clientX;                                      
-    yDown = firstTouch.clientY;                                      
-};                                                
+// function handleTouchStart(evt) {
+//     const firstTouch = getTouches(evt)[0];                                      
+//     xDown = firstTouch.clientX;                                      
+//     yDown = firstTouch.clientY;                                      
+// };                                                
 
 
-function handleTouchMove(evt) {
-    if ( ! xDown || ! yDown ) {
-        return;
-    }
+// function handleTouchMove(evt) {
+//     if ( ! xDown || ! yDown ) {
+//         return;
+//     }
 
-    let xUp = evt.touches[0].clientX;                                    
-    let yUp = evt.touches[0].clientY;
+//     let xUp = evt.touches[0].clientX;                                    
+//     let yUp = evt.touches[0].clientY;
 
-    let xDiff = xDown - xUp;
-    let yDiff = yDown - yUp;
+//     let xDiff = xDown - xUp;
+//     let yDiff = yDown - yUp;
 
-    if ( Math.abs( xDiff ) > Math.abs( yDiff ) &&  !(modal_window.classList.contains('open')) ) { //проверка на открытое модальное окно
-      if (xDiff > 0 && second_item.classList.contains('sliders__item_active')){
-          second_item.classList.remove('sliders__item_active');
-          third_item.classList.add('sliders__item_active');
-          content.classList.remove('sliders_second');
-          content.classList.add('sliders_third');
-      } else if ( xDiff > 0 && first_item.classList.contains('sliders__item_active')) {
-          first_item.classList.remove('sliders__item_active');
-          second_item.classList.add('sliders__item_active');
-          content.classList.remove('sliders_first');
-          content.classList.add('sliders_second')
-      } else if (xDiff < 0 && third_item.classList.contains('sliders__item_active')){
-          third_item.classList.remove('sliders__item_active');
-          second_item.classList.add('sliders__item_active');
-          content.classList.remove('sliders_third');
-          content.classList.add('sliders_second')
-      } else if (xDiff < 0 && second_item.classList.contains('sliders__item_active')) {
-          first_item.classList.add('sliders__item_active');
-          second_item.classList.remove('sliders__item_active');
-          content.classList.remove('sliders_second');
-          content.classList.add('sliders_first')
-      }               
-  } 
+//     if ( Math.abs( xDiff ) > Math.abs( yDiff ) &&  !(modal_window.classList.contains('open')) ) { //проверка на открытое модальное окно
+//         if (xDiff > 0 && second_item.classList.contains('sliders__item_active')){;
+//             third_item.classList.add('sliders__item_active');
+//         } else if ( xDiff > 0 && first_item.classList.contains('sliders__item_active')) {
+//             second_item.classList.add('sliders__item_active');
+//         } else if (xDiff < 0 && third_item.classList.contains('sliders__item_active')){
+//             second_item.classList.add('sliders__item_active');
+//         } else if (xDiff < 0 && second_item.classList.contains('sliders__item_active')) {
+//             first_item.classList.add('sliders__item_active');
+//         }               
+//     } 
 
-    xDown = null;                                             
-};
+//     xDown = null;                                             
+// };
 
 //modal window
 
@@ -122,13 +105,14 @@ modals.forEach(function(trigger) {
 
 const left_arrow = document.getElementById('left-arrow');
 const right_arrow = document.getElementById('right-arrow');
-const firstList = document.getElementById('first-list');
-const secondList = document.getElementById('second-list');
-const firstBall = document.getElementById('first-ball');
-const secondBall = document.getElementById('second-ball');
 
 function swapBenefits() {
-  right_arrow.addEventListener('click', () => {
+  right_arrow.addEventListener('click', function(e) {
+    e.preventDefault();
+    const firstList = document.getElementById('first-list');
+    const secondList = document.getElementById('second-list');
+    const firstBall = document.getElementById('first-ball');
+    const secondBall = document.getElementById('second-ball');
 
     secondList.classList.add('modal__content_active');
     secondBall.classList.add('modal__switches__ball_active');
@@ -136,7 +120,12 @@ function swapBenefits() {
     firstBall.classList.remove('modal__switches__ball_active');
     
   })
-  left_arrow.addEventListener('click', () => {
+  left_arrow.addEventListener('click', function(e) {
+    e.preventDefault();
+    const firstList = document.getElementById('first-list');
+    const secondList = document.getElementById('second-list');
+    const firstBall = document.getElementById('first-ball');
+    const secondBall = document.getElementById('second-ball');
 
     secondList.classList.remove('modal__content_active');
     secondBall.classList.remove('modal__switches__ball_active');
@@ -147,3 +136,14 @@ function swapBenefits() {
 }
 
 swapBenefits();
+
+//scroll 
+
+function scroll() {
+  const scroll_thumb = document.getElementById('scroll-thumb');
+  if (scroll_thumb) {
+    scroll_thumb.addEventListener('mousedown', () => {
+      console.log('hey')
+    })
+  }
+}
